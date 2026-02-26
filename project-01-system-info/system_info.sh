d36b5a6cf448256bc28c7c964e63346c2f4d5a89
Yasmin478 <yasmin4788@gmail.com>
2026-02-26 13:50:53 +0530

    Improved system info script with logging

--- project-01-system-info/system_info.sh
+++ project-01-system-info/system_info.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+
+LOGFILE="system_info.log"
+echo "------------------------------------------" >> $LOGFILE
+echo "System Report - $(date)" >> $LOGFILE
+echo "------------------------------------------" >> $LOGFILE
+
+echo "Uptime:" >> $LOGFILE
+uptime >> $LOGFILE
+
+echo "" >> $LOGFILE
+echo "Memory Usage:" >> $LOGFILE
+free -m >> $LOGFILE
+
+echo "" >> $LOGFILE
+echo "Disk Usage:" >> $LOGFILE
+df -h >> $LOGFILE
+
+echo "" >> $LOGFILE
+echo "CPU Load:" >> $LOGFILE
+top -bn1 | grep "load average" >> $LOGFILE
+
+echo "" >> $LOGFILE
+
+
+
 echo "System Information"
 echo "------------------"
 echo "Hostname: $(hostname)"
@@ -11,6 +36,7 @@ echo "Memory Usage:" $(free -h | awk '/^Mem:/ {print $3}')
 echo "Memory Used: $(free -m | awk '/^Mem:/ {printf "%.2f%%\n", ($3/$2)*100}')"
 echo "Disk Usage: $(df -h / | awk 'NR==2 {print $5}')"
 echo "Uptime: $(uptime -p)"
-echo "Current Users: $(who | wc -l)"
+echo "Current User: $(whoami)"
+echo "Current no. of Users: $(who| wc -l)"
 echo "Current Date and Time: $(date)"
 echo "IP Address: $(hostname -I | awk '{print $1}')"
