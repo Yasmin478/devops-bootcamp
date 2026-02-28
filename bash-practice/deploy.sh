411de9eccec9fa9f96d06d30dc7685f2829b9182
Yasmin478 <yasmin4788@gmail.com>
2026-02-28 15:24:18 +0530

    Updated with case statement

--- bash-practice/deploy.sh
+++ bash-practice/deploy.sh
@@ -2,14 +2,23 @@
 
 env=$1
 
-if [ -z "$env" ]; then
-	echo "Usage: ./deploy.sh <dev|staging|production>"
+if [ -z "$env" ] ; then
+	echo "Usage: $0 {dev|staging|production}"
 	exit 1
 fi
 
-if [ "$env" != "dev" ] && [ "$env" != "staging" ] && [ "$env" != "production" ]; then
-	echo "Invalid envireonment!"
-	exit 1
-fi
-
-echo "Deploying application to $env environment..." 
+case "${env,,}" in
+	dev)
+		echo "Deploying application to DEV environment"
+		;;
+	staging)
+		echo "Deploying application to STAGING environment"
+		;;
+	production)
+		echo "Deploying application to PRODUCTION environment"
+		;;
+	*)
+		echo "Usage: $0 {dev|staging|production}"
+		exit 1
+		;;
+esac
