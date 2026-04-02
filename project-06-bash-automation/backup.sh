c269e1da42e01d10ac021c83897176559f951fc7
Yasmin478 <yasmin4788@gmail.com>
2026-04-02 15:42:18 +0530

    feat: initial backup automation with tar compression

---
+++ project-06-bash-automation/backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+SOURCE_DIR="${1:-}"
+BACKUP_DIR="./backups"
+
+
+#----Validate input----
+if [[ -z "$SOURCE_DIR" ]]; then
+    echo "Usage: $0 <directory_to_backup>"
+    exit 1
+fi  
+
+if [[ ! -d "$SOURCE_DIR" ]]; then
+    echo "Error: Directory does not exist."
+    exit 1
+fi
+
+#----Create backup directory if it doesn't exist----
+mkdir -p "$BACKUP_DIR"
+
+#----Create backup file name----
+BACKUP_FILE="$BACKUP_DIR/$(basename "$SOURCE_DIR").tar.gz"
+
+#----Create backup---
+tar -czf "$BACKUP_FILE" "$SOURCE_DIR"
+
+echo "Backup created successfully at: $BACKUP_FILE"
