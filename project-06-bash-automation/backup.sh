5d726433ba05d2465ac81444c37e241e9491235e
Yasmin478 <yasmin4788@gmail.com>
2026-04-02 16:14:17 +0530

    feat: add timestamp-based naming to prevent backup overwrite

--- project-06-bash-automation/backup.sh
+++ project-06-bash-automation/backup.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 SOURCE_DIR="${1:-}"
 BACKUP_DIR="./backups"
 
-
 #----Validate input----
 if [[ -z "$SOURCE_DIR" ]]; then
     echo "Usage: $0 <directory_to_backup>"
@@ -19,8 +18,14 @@ fi
 #----Create backup directory if it doesn't exist----
 mkdir -p "$BACKUP_DIR"
 
+#----Generate timestamp----
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+
+#----Extract directory name for backup file----
+BASENAME=$(basename "$SOURCE_DIR")
+
 #----Create backup file name----
-BACKUP_FILE="$BACKUP_DIR/$(basename "$SOURCE_DIR").tar.gz"
+BACKUP_FILE="$BACKUP_DIR/${BASENAME}_${TIMESTAMP}.tar.gz"
 
 #----Create backup---
 tar -czf "$BACKUP_FILE" "$SOURCE_DIR"
