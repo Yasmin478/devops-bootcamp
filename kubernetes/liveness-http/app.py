79288d3aefd9595317ea27f095fae535775978c7
Yasmin478 <yasmin4788@gmail.com>
2026-07-11 12:14:06 +0530

    feat: add HTTP readiness probe for Flask application

--- kubernetes/liveness-http/app.py
+++ kubernetes/liveness-http/app.py
@@ -1,20 +1,28 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
-
-import os
 
 @app.route("/")
 def home():
     return "Hello from Kubernetes!"
 
-@app.route("/health")
-def health():
-    if os.path.exists("/tmp/healthy"):
-        return "OK", 200
-    return "FAILED", 500
+@app.route("/live")
+def live():
+    if os.path.exists("/tmp/live"):
+        return "Alive", 200
+    return "Dead", 500
+
+
+@app.route("/ready")
+def ready():
+    if os.path.exists("/tmp/ready"):
+        return "Ready", 200
+    return "Not Ready", 503
 
 
 if __name__ == "__main__":
-    open("/tmp/healthy","w").close()
+    open("/tmp/live","w").close()
+    open("/tmp/ready", "w").close()
+    
     app.run(host="0.0.0.0", port=5000)
